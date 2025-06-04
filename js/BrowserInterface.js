f94adba1cf29e25782a500cccb94d057ed71cbe4
Rafal Fronczyk <rfronczyk@gmail.com>
2025-06-04 12:45:33 +0200

    Remove Tomek Ch.

--- js/BrowserInterface.js
+++ js/BrowserInterface.js
@@ -18,7 +18,7 @@
 
   // Shuffle card images: How many different images are available to shuffle
   // from?
-  var imagesAvailable = 14;
+  var imagesAvailable = 13;
 
   /************ End hard coded settings ******************/
 
